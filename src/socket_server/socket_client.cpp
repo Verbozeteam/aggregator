dade796ceabbb21aa7621e6afe0b06681b175895
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2017-10-29 18:23:41 +0300

    Message reception from socket complete

--- src/socket_server/socket_client.cpp
+++ src/socket_server/socket_client.cpp
@@ -21,9 +21,32 @@ bool SocketClient::OnReadingAvailable() {
         return false;
     } else {
         m_read_buffer.insert(std::end(m_read_buffer), &tmp_buf[0], &tmp_buf[rbytes]);
-        if (!OnRead()) {
-            LOG(warning) << "Client " << m_client_ip << " (fd " << m_client_fd << ") communication failure";
-            return false;
+        if (m_read_buffer.size() >= 4) {
+            size_t payload_size =
+                ((((size_t)m_read_buffer[0]) & 0xFF)      ) |
+                ((((size_t)m_read_buffer[1]) & 0xFF) << 8 ) |
+                ((((size_t)m_read_buffer[2]) & 0xFF) << 16) |
+                ((((size_t)m_read_buffer[3]) & 0xFF) << 24);
+            if (m_read_buffer.size() >= 4 + payload_size) {
+                // create a buffer containing the JSON message
+                char* s = new char[payload_size+1];
+                memcpy(s, &m_read_buffer[4], payload_size);
+                s[payload_size] = 0;
+                m_read_buffer.erase(m_read_buffer.begin(), m_read_buffer.begin() + 4 + payload_size);
+                json j;
+                try {
+                    j = json::parse(s);
+                    if (j.is_null())
+                        throw;
+                } catch (...) {
+                    LOG(warning) << "Client " << m_client_ip << " sent invalid JSON " << s;
+                }
+                delete[] s;
+                if (j.is_null() || j.size() == 0 || !OnMessage(j)) {
+                    LOG(warning) << "Client " << m_client_ip << " (fd " << m_client_fd << ") communication failure";
+                    return false;
+                }
+            }
         }
     }
 
@@ -42,9 +65,17 @@ bool SocketClient::OnWritingAvailable() {
 }
 
 void SocketClient::Write(json msg) {
-
+    std::string dump = msg.dump();
+    const char* cstr = dump.c_str();
+    size_t payload_size = strlen(cstr);
+    m_write_buffer.push_back((unsigned char)((payload_size      ) & 0xFF));
+    m_write_buffer.push_back((unsigned char)((payload_size >> 8 ) & 0xFF));
+    m_write_buffer.push_back((unsigned char)((payload_size >> 16) & 0xFF));
+    m_write_buffer.push_back((unsigned char)((payload_size >> 24) & 0xFF));
+    m_write_buffer.insert(std::end(m_write_buffer), &cstr[0], &cstr[payload_size]);
 }
 
-bool SocketClient::OnRead() {
+bool SocketClient::OnMessage(json msg) {
+    LOG(trace) << "Received message from " << m_client_ip << ": " << msg;
     return true;
 }
--- src/socket_server/socket_client.hpp
+++ src/socket_server/socket_client.hpp
@@ -7,21 +7,26 @@
 using json = nlohmann::json;
 
 class SocketClient {
+    /**
+     * Internal use in harmony with the SocketServer to perform IO to the socket
+     */
     friend class SocketServer;
 
     int m_client_fd;
-    std::string m_client_ip;
     std::vector<unsigned char> m_write_buffer;
     std::vector<unsigned char> m_read_buffer;
 
     bool OnReadingAvailable();
     bool OnWritingAvailable();
 
+protected:
+    std::string m_client_ip;
+
 public:
     SocketClient(int fd, std::string ip);
     ~SocketClient();
 
     void Write(json msg);
-    bool OnRead();
+    bool OnMessage(json msg);
 };
 
